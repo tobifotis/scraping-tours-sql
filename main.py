5358178e15b2b0ce6f4cebae4dd2e08bcc1c0ddc
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-23 12:58:54 -0500

    feat: add automated email notification and infinite loop for continuous scraping

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import selectorlib
 
+import smtplib, ssl
+import os
+
+import time
+
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -19,7 +24,22 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = os.getenv("EMAIL_USER")
+    password = os.getenv("EMAIL_PASS")
+
+
+
+    receiver = "Tobiemanuel25@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
     print("Email Sent!")
 
 
@@ -27,20 +47,24 @@ def store(extracted):
     with open("data.txt", "a") as file:
         file.write(extracted + "\n")
 
+
 def read(extracted):
     with open("data.txt", "r") as file:
         return file.read()
 
+
 if __name__ == "__main__":
-    scraped = scrape(URL)
+    while True:
+        scraped = scrape(URL)
 
-    extracted = extract(scraped)
-    print(extracted)
+        extracted = extract(scraped)
+        print(extracted)
 
+        content = read(extracted)
 
-    content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hi!")
 
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+        time.sleep(2)
