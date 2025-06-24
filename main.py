6ce9f98099ab5c90c6efb7a6e22a265d25d1bd07
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-24 13:14:11 -0500

    feat: integrate SQLite to store and check for duplicate events before sending notifications

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import os
 
 import time
 
+import sqlite3
+
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish a connection and a cursor
+connection = sqlite3.connect("data.db")
 
 
 def scrape(url):
@@ -31,8 +36,6 @@ def send_email(message):
     username = os.getenv("EMAIL_USER")
     password = os.getenv("EMAIL_PASS")
 
-
-
     receiver = "Tobiemanuel25@gmail.com"
     context = ssl.create_default_context()
 
@@ -44,13 +47,24 @@ def send_email(message):
 
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+
+    # Query all data based on a condition
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 
 if __name__ == "__main__":
@@ -60,11 +74,12 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
 
-        content = read(extracted)
 
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+
+            if not row:
                 store(extracted)
-                send_email(message="Hi!")
+                send_email(message="Hey, new event was found!")
 
         time.sleep(2)
